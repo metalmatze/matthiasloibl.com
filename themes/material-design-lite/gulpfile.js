96de26a650c8093b9840f648bc6a403797ceb80e
Matthias Loibl <mail@matthiasloibl.com>
2016-02-06 20:44:35 +0100

    Update npm deps and gulp

--- themes/material-design-lite/gulpfile.js
+++ themes/material-design-lite/gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 gulp.task('default', [
     'js',
     'css',
+    'concat',
     'copy'
 ]);
 
@@ -27,6 +29,15 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./static/css'));
 });
 
+gulp.task('concat', function () {
+    return gulp.src([
+            'node_modules/particles.js/particles.js',
+            'assets/js/particles.js'
+        ])
+        .pipe(concat('particles.js'))
+        .pipe(gulp.dest('static/js'));
+});
+
 gulp.task('minify', ['default'], function () {
     //gulp.src('static/app.js')
     //    .pipe(uglify())
@@ -39,6 +50,6 @@ gulp.task('minify', ['default'], function () {
 gulp.task('copy', function () {
     gulp.src('node_modules/material-design-lite/material.min.js')
         .pipe(gulp.dest('static/js'));
-    //gulp.src('node_modules/material-design-icons/iconfont/*')
-    //    .pipe(gulp.dest('static/fonts'))
+    gulp.src('node_modules/mobile-detect/mobile-detect.min.js')
+        .pipe(gulp.dest('static/js'));
 });
